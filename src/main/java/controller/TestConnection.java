3614df21144fb533df7c66007250f07e996ecf53
FractureDVL <joseandrescram1005@gmail.com>
2021-10-21 09:19:20 -0500

    Added close connection

--- src/main/java/controller/TestConnection.java
+++ src/main/java/controller/TestConnection.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class TestConnection {
 
@@ -15,24 +13,28 @@ public class TestConnection {
         String db = "kaktus";
         String user = "root";
         String pass = "admin";
-        String url = "jdbc:mysql://localhost:3306/" + db + "?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String url = "jdbc:mariadb://localhost:3306/" + db + "?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
         try {
             Connection con = DriverManager.getConnection(url, user, pass);
             Statement sentencia = con.createStatement();
+            
             String sql = "SELECT id_user,name, email, nickname,password,image_url,role FROM users";
             ResultSet resultado = sentencia.executeQuery(sql);
             while (resultado.next()){
                 
                 System.out.println("Usuario :"+resultado.getString(2)+""
                         + "       " + "Email:"+resultado.getString(3));
-            
-               
+                
             }
+            resultado.close();
+            sentencia.close();
+            con.close();
+
         } catch (SQLException ex) {
                 System.out.println("-----------------------------------------------");
                 System.out.println("No se pudo conectar con la base de datos");
-                System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣤\n" +
+                System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣤⣤\n" +
                                     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿\n" +
                                     "⠀⠀⣶⠀⠀⣀⣤⣶⣤⣉⣿⣿⣤⣀\n" +
                                     "⠤⣤⣿⣤⣿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣀\n" +
