41b8d82719de70fd35662daea89e92bfcf624d36
FractureDVL <joseandrescram1005@gmail.com>
2021-10-22 15:07:28 -0500

    Modified UsuarioServlet

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package controller;
 
 import dao.UsuarioDAO;
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,30 +11,37 @@ import javax.servlet.http.HttpServletResponse;
 import modelo.UsuarioDTO;
 
 @WebServlet("/register")
-public class UsuarioController extends HttpServlet{
+public class UsuarioController extends HttpServlet {
+
     
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-       String accion = request.getParameter("accion");
-       if (accion != null){
-       switch(accion){
-           case "insertar":
-               this.insertar(request, response);
-               break;
-           case "modificar":
-               break;
-       }
-       }       
+    private UsuarioDAO u = new UsuarioDAO();
+    
+    //Contructor
+    
+    public UsuarioController(){
+        super();
     }
     
-    private void insertar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       String name = request.getParameter("name");
-       String email = request.getParameter("email");
-       String user = request.getParameter("user");
-       String password = request.getParameter("password");
-              
-       UsuarioDTO usuario = new UsuarioDTO(name, email, user, password);                   
-       int registros = new UsuarioDAO().insertar(usuario);
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.getWriter().append("Server at: ").append(request.getContextPath());
+        RequestDispatcher dispacher = request.getRequestDispatcher("webapp/login.html");
+        dispacher.forward(request, response);
+    
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+
+        UsuarioDTO usuario = new UsuarioDTO(name, email, user, password);
+        
+        u.crearUsuario(usuario);
+        
+        response.sendRedirect("registro.jsp");
     }
     
 }
