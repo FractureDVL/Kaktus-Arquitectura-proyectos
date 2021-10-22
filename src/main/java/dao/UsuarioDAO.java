a09a9b474d8e6508fdf13f3db691bf20639a96d7
FractureDVL <joseandrescram1005@gmail.com>
2021-10-22 15:11:03 -0500

    Modified UsuarioDAO and added crearUsuario method

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -1,48 +1,72 @@
 package dao;
 
-import controller.ConnectionBD;
+import controller.ConnectionDB;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import modelo.UsuarioDTO;
 
 public class UsuarioDAO implements UsuarioServices{
     
-   public static final String SQL_INSERTAR = "INSERT INTO users (id_user, name, email, nickname, password, image_url, role) VALUES(?,?,?,?,?,?,?)";
-   
-   
-   
-   public int insertar(UsuarioDTO u){
-       Connection con = null;
-       PreparedStatement sen = null;
-       int registros = 0;
-      
-       try{
-           con = ConnectionBD.getConnection();
-           sen = con.prepareStatement(SQL_INSERTAR);
-           sen.setInt(1, u.getId());
-           sen.setString(2, u.getName());
-           sen.setString(3, u.getEmail());
-           sen.setString(4, u.getUser());
-           sen.setString(5, u.getPassword());
-           sen.setString(6, u.getImgUrl());
-           sen.setString(7, u.getRole());
-           
-           registros = sen.executeUpdate();
-       }
-       catch(SQLException ex){
-           ex.printStackTrace(System.out);
-       }
-       
-       finally{
+  private static final String GET_ALL = "SELECT id_user, name, email, nickname,image_url, role FROM users";
+    private static final String SQL_CREATE_USER = "INSERT INTO `users`(`name`, `email`, `nickname`, `password`) VALUES (?,?,?,?)";
+    private static final String SQL_UPDATE_PASS = "UPDATE users SET password = ? WHERE id_user = ?";
+
+    public int crearUsuario(UsuarioDTO u) {
+
+        return this.ejecutarSQL(u, 1);
+    }
+
+    public int changePassword(UsuarioDTO u) {
+
+        return this.ejecutarSQL(u, 2);
+    }
+
+    private int ejecutarSQL(UsuarioDTO u, int s) {
+
+        Connection con = null;
+        PreparedStatement ps = null;
+        int registros = 0;
+        
+        try {
+            con = ConnectionDB.getConnection();
+
+            switch (s) {
+                case 1: {
+                    ps = con.prepareStatement(this.SQL_CREATE_USER);
+                    ps.setString(1, u.getName());
+                    ps.setString(2, u.getEmail());
+                    ps.setString(3, u.getUser());
+                    ps.setString(4, u.getPassword());
+                    
+                    break;
+                }
+//                case 2: {
+//
+//                    ps = con.prepareCall(SQL_UPDATE_PASS);
+//                    ps.setString(5, u.getPassword());
+//                    break;
+//
+//                }
+            }
+            registros = ps.executeUpdate();
+
+        } catch (SQLException ex) {
+           Logger.getLogger(UsuarioDTO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("No se logro introducir los datos");
+        } finally {
+
             try {
-                ConnectionBD.close(sen);
-                ConnectionBD.close(con);
+                ConnectionDB.close(ps);
+                ConnectionDB.close(con);
+
             } catch (SQLException ex) {
-                ex.printStackTrace(System.out);
+                Logger.getLogger(UsuarioDTO.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-    
+
         return registros;
-   }
+    }
 }
